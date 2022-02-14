d772dcec44e837ce40405337232412b358252c58
sddd3 <gohoshiro@gmail.com>
2022-02-14 23:47:21 +0900

    【Rust】C - Otoshidama

--- rust/c_otoshidama/src/main.rs
+++ rust/c_otoshidama/src/main.rs
@@ -7,24 +7,14 @@ fn main() {
         y: u64,
     }
     let mut answer = String::new();
-    let mut a = 0;
-    loop {
-        let mut b = 0;
-        loop {
+    for a in 0..n + 1 {
+        for b in 0..n - a + 1 {
             let c = n - a - b;
             let total = a * 10000 + b * 5000 + c * 1000;
             if total == y {
                 answer = a.to_string() + " " + &b.to_string() + " " + &c.to_string();
             }
-            if a + b == n {
-                break;
-            }
-            b += 1;
         }
-        if a == n {
-            break;
-        };
-        a += 1;
     }
 
     if answer == "" {
